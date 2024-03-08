e06d8f7f84a5b66e4d409ca882645fd8d69c4985
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-08 23:16:24 +0300

    Update vcplayer.py

--- vcplayer.py
+++ vcplayer.py
@@ -3,7 +3,7 @@ import logging
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon.tl.types import User
-from BATT import Config, lucmd9
+from BATT import Config,lucmd9
 from BATT.core.managers import edit_delete, edit_or_reply
 
 from .helper.stream_helper import Stream
